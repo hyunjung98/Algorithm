ceebc68155537618eca36a13e2f2f3de8c56a3df
hyunjung98 <hjk2754@gmail.com>
2022-12-12 02:14:47 +0900

    블랙잭

---
+++ BOJ/2798/2798.cpp
@@ -0,0 +1,44 @@
+﻿#include <iostream>
+
+using namespace std;
+
+#define MAX 101
+
+int N, M;
+int sum;
+int result = 0;
+
+int blackJack[MAX];
+
+int main()
+{
+	ios::sync_with_stdio(false);
+	cin.tie(0);
+
+	// 카드의 개수와 딜러가 외친 숫자 입력 받기
+	cin >> N >> M;
+
+	for (int i = 0; i < N; i++)
+	{
+		cin >> blackJack[i];
+	}
+
+	for (int i = 0; i < N; i++)
+	{
+		for (int j = i + 1; j < N; j++)
+		{
+			for (int k = j + 1; k < N; k++)
+			{
+				sum = blackJack[i] + blackJack[j] + blackJack[k];
+				if (sum <= M)
+				{
+					result = max(result, sum);	// 둘 중 큰 값을 반환
+				}
+			}
+
+		}
+	}
+
+	cout << result;
+	return 0;
+}
