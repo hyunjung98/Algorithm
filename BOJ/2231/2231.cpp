02b353192aed3da42e07593e86ad3edd98453ee3
hyunjung98 <hjk2754@gmail.com>
2022-12-13 00:33:37 +0900

    분해합

---
+++ BOJ/2231/2231.cpp
@@ -0,0 +1,33 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int N, decompos, digits;
+
+int main()
+{
+	ios::sync_with_stdio(false);
+	cin.tie(0);
+
+	cin >> N;
+
+	for (int i = 1; i < N; i++)
+	{
+		int temp = i;
+		int sum = i;
+
+		while (temp > 0)
+		{
+			sum += temp % 10;
+			temp /= 10;
+		}
+
+		if (sum == N)
+		{
+			cout << i;
+			return 0;
+		}
+	}
+	cout << 0;
+	return 0;
+}
