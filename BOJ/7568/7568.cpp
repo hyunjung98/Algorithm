0e06b9aa57c16dcfffc391a32e9c9d6161785f63
hyunjung98 <hjk2754@gmail.com>
2022-12-14 16:22:42 +0900

    덩치

---
+++ BOJ/7568/7568.cpp
@@ -0,0 +1,35 @@
+﻿#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int N, weight, height;
+vector<pair<int, int>> v;
+
+int main()
+{
+	ios::sync_with_stdio(false);
+	cin.tie(0);
+
+	cin >> N;
+	for (int i = 0; i < N; i++)
+	{
+		cin >> weight >> height;
+		v.push_back({ weight, height });
+	}
+
+	for (int i = 0; i < N; i++)
+	{
+		int rank = 1;
+		for (int j = 0; j < N; j++)
+		{
+			if (v[i].first < v[j].first && v[i].second < v[j].second)
+			{
+				rank++;
+			}
+		}
+		cout << rank << " ";
+	}
+
+	return 0;
+}
