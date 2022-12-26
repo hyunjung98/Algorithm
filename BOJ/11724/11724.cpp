48a0d9fd7dad71ebbb0f77e5c949b676d4e7ae59
hyunjung98 <hjk2754@gmail.com>
2022-12-26 18:00:00 +0900

    연결 요소의 개수

---
+++ BOJ/11724/11724.cpp
@@ -0,0 +1,51 @@
+﻿#include <iostream>
+#include <vector>
+
+using namespace std;
+
+#define MAX 1001
+
+vector<int> vec[MAX];
+bool visited[MAX];
+
+int vertex, edge, from, to, cnt;
+
+void Dfs(int v)
+{
+	visited[v] = true;
+
+	for (int i = 0; i < vec[v].size(); i++)
+	{
+		int next = vec[v][i];
+		if (!visited[next])
+			Dfs(next);
+	}
+}
+
+int main()
+{
+	ios::sync_with_stdio(false);
+	cin.tie(0); cout.tie(0);
+
+	cin >> vertex >> edge;
+
+	for (int i = 1; i <= edge; i++)
+	{
+		cin >> from >> to;
+		vec[from].push_back(to);
+		vec[to].push_back(from);
+	}
+
+	for (int i = 1; i <= vertex; i++)
+	{
+		if (!visited[i])
+		{
+			Dfs(i);
+			cnt++;
+		}
+	}
+
+	cout << cnt;
+
+	return 0;
+}
