6ba6f30dacb0594fd782377418d4c1166b2f2bd1
hyunjung98 <hjk2754@gmail.com>
2023-01-19 20:50:14 +0900

    Update 2573.cpp

--- BOJ/2573/2573.cpp
+++ BOJ/2573/2573.cpp
@@ -9,7 +9,7 @@ using namespace std;
 #define _Y second
 
 int map[MAX][MAX];
-int copyMap[MAX][MAX];
+int copyMap[MAX][MAX];	// 현재 배열을 저장하여 빙산을 녹일 때 사용
 bool visited[MAX][MAX];
 int dx[] = { 1, -1, 0, 0 };
 int dy[] = { 0, 0, -1, 1 };
@@ -41,6 +41,7 @@ void Bfs()
 	}
 }
 
+// 빙산이 다 녹았으면 참, 아니면 거짓 반환하는 함수
 bool HasAllMelt()
 {
 	for (int i = 0; i < N; i++)
@@ -53,6 +54,8 @@ bool HasAllMelt()
 	return true;
 }
 
+// 빙산을 녹이는 함수
+// 빙산이 존재하는 위치의 상하좌우를 탐색하여 주변 바다의 수를 저장하여 녹이고, 년수를 증가시킨다
 void MeltIceberg()
 {
 	for (int i = 0; i < N; i++)
@@ -93,8 +96,7 @@ int main()
 		if (HasAllMelt()) break;
 
 		int cnt = 0;
-		int result = 0;
-		
+
 		memset(visited, false, sizeof(visited));
 
 		for (int i = 0; i < N; i++)
@@ -108,7 +110,7 @@ int main()
 					Bfs();
 					cnt++;
 				}
-				copyMap[i][j] = map[i][j];
+				copyMap[i][j] = map[i][j];	// 현재 배열 저장
 			}
 		}
 
