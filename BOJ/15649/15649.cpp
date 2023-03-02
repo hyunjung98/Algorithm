f0207807dd6cd641976579e2ff5f1a73adf032fc
hyunjung98 <hjk2754@gmail.com>
2023-03-03 08:59:36 +0900

    N과 M (1)

---
+++ BOJ/15649/15649.cpp
@@ -0,0 +1,41 @@
+﻿#include <iostream>
+
+using namespace std;
+
+#define MAX 10
+
+int N, M;
+int arr[MAX];
+bool isUsed[MAX];
+
+void Dfs(int cnt) 
+{
+	if (cnt == M)
+	{
+		for (int i = 0; i < M; i++)
+		{
+			cout << arr[i] << ' ';
+		}
+		cout << '\n';
+		return;
+	}
+
+	for (int i = 1; i <= N; i++)
+	{
+		if (isUsed[i]) continue;
+		arr[cnt] = i;
+		isUsed[i] = true;
+		Dfs(cnt + 1);
+		isUsed[i] = false;
+	}
+}
+
+int main()
+{
+	ios::sync_with_stdio(false);
+	cin.tie(0); cout.tie(0);
+	cin >> N >> M;
+	Dfs(0);
+
+	return 0;
+}
