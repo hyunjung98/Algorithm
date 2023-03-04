389b401ad2c66ae6e5f832b09db8b3fe4c5f6d11
hyunjung98 <hjk2754@gmail.com>
2023-03-04 09:10:22 +0900

    N과 M (8)

---
+++ BOJ/15657/15657.cpp
@@ -0,0 +1,45 @@
+﻿#include <iostream>
+#include <algorithm>
+
+using namespace std;
+#define MAX 10
+
+int N, M;
+int arr[MAX];
+int aws[MAX];
+
+void func(int num, int cnt)
+{
+	if (cnt == M)
+	{
+		for (int i = 0; i < M; i++)
+		{
+			cout << aws[i] << ' ';
+		}
+		cout << '\n';
+		return;
+	}
+
+	for (int i = num; i < N; i++)
+	{
+		aws[cnt] = arr[i];
+		func(i, cnt + 1);
+	}
+}
+
+int main()
+{
+	ios::sync_with_stdio(false);
+	cin.tie(0); cout.tie(0);
+
+	cin >> N >> M;
+	for (int i = 0; i < N; i++)
+	{
+		cin >> arr[i];
+	}
+
+	sort(arr, arr + N);
+	func(0, 0);
+
+	return 0;
+}
