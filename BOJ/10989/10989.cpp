22380d45aeb7af2ef0afee0576361bd4f2b217d9
hyunjung98 <hjk2754@gmail.com>
2023-03-04 14:54:46 +0900

    수 정렬하기 3

---
+++ BOJ/10989/10989.cpp
@@ -0,0 +1,32 @@
+﻿#include <iostream>
+#include <algorithm>
+
+using namespace std;
+#define MAX 10001
+
+
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(NULL); cout.tie(NULL);
+    int N;
+    int arr[MAX] = { 0, };
+    cin >> N;
+
+    for (int i = 0; i < N; i++)
+    {
+        int value;
+        cin >> value;
+        arr[value]++;
+    }
+
+    for (int i = 0; i < MAX; i++)
+    {
+        for (int j = 0; j < arr[i]; j++)
+        {
+            cout << i << '\n';
+        }
+    }
+
+    return 0;
+}
