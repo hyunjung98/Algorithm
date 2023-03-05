de7b50f6c4d785d4538b0e5fc50af9e55cfb02c1
hyunjung98 <hjk2754@gmail.com>
2023-03-05 18:13:05 +0900

    회의실 배정

---
+++ BOJ/1931/1931.cpp
@@ -0,0 +1,33 @@
+﻿#include <iostream>
+#include <algorithm>
+using namespace std;
+
+int n;
+pair<int, int> pTime[100001];
+
+int main()
+{
+	ios::sync_with_stdio(false);
+	cin.tie(0); cout.tie(0);
+
+	cin >> n;
+	for (int i = 0; i < n; i++)
+	{
+		cin >> pTime[i].second >> pTime[i].first;
+	}
+
+	sort(pTime, pTime + n);		// 끝나는 시간 기준 오름차순 정렬
+	
+	int ans = 0;
+	int t = 0;
+
+	for (int i = 0; i < n; i++)
+	{
+		if (t > pTime[i].second) continue;
+			t = pTime[i].first;
+			ans++;
+	}
+	cout << ans;
+
+	return 0;
+}
