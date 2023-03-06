9a3a935c5bec414d6a4b990c06d1278f12f09b42
hyunjung98 <hjk2754@gmail.com>
2023-03-06 11:44:14 +0900

    팩토리얼

---
+++ BOJ/10872/10872.cpp
@@ -0,0 +1,24 @@
+﻿#include <iostream>
+using namespace std;
+
+int factorial(int k)
+{
+    if (k <= 1)
+        return 1;
+    else
+        return k * factorial(k-1);
+}
+
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(0); cout.tie(0);
+
+    int n;
+    cin >> n;
+    
+    int ans = factorial(n);
+    cout << ans;
+
+    return 0;
+}
