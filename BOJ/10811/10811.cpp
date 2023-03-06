f152270907ee24af0e9ba8dc2a01181701f2ad97
hyunjung98 <hjk2754@gmail.com>
2023-03-06 18:55:33 +0900

    바구니 뒤집기

---
+++ BOJ/10811/10811.cpp
@@ -0,0 +1,32 @@
+﻿#include <iostream>
+#include <algorithm>
+
+using namespace std;
+int n, m;
+int arr[101] = { 0, };
+
+int main()
+{
+	ios::sync_with_stdio(false);
+	cin.tie(0); cout.tie(0);
+
+	cin >> n >> m;
+
+	for (int i = 0; i < n; i++)
+	{
+		arr[i] = i + 1;
+	}
+
+	for (int i = 0; i < m; i++)
+	{
+		int start, end;
+		cin >> start >> end;
+		reverse(arr + start - 1, arr + end);
+	}
+
+	for (int i = 0; i < n; i++)
+	{
+		cout << arr[i] << ' ';
+	}
+	return 0;
+}
