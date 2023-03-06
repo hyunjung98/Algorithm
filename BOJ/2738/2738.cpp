99eab95f5349ae37ded395ccd3d7e31de70a5ffb
hyunjung98 <hjk2754@gmail.com>
2023-03-06 20:23:36 +0900

    행렬 덧셈

---
+++ BOJ/2738/2738.cpp
@@ -0,0 +1,39 @@
+﻿#include <iostream>
+using namespace std;
+#define MAX 100
+int n, m;
+int proc1[MAX][MAX];
+int proc2[MAX][MAX];
+int sum[MAX][MAX];
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(0); cout.tie(0);
+
+    cin >> n >> m;
+
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < m; j++)
+        {
+            cin >> proc1[i][j];
+        }
+    }
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < m; j++)
+        {
+            cin >> proc2[i][j];
+        }
+    }
+
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < m; j++)
+        {
+            cout << proc1[i][j] + proc2[i][j] << ' ';
+        }
+        cout << '\n';
+    }
+    return 0;
+}
