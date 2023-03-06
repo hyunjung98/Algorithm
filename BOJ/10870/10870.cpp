9ed8d2fb61c2e1cc90dd7735296b4098a4856377
hyunjung98 <hjk2754@gmail.com>
2023-03-06 11:44:25 +0900

    피보나치 수 5

---
+++ BOJ/10870/10870.cpp
@@ -0,0 +1,27 @@
+﻿#include <iostream>
+using namespace std;
+
+int n;
+
+int Fibonacci(int k)
+{
+    if (k == 0)
+        return 0;
+    else if (k == 1)
+        return 1;
+    else
+        return Fibonacci(k-1) + Fibonacci(k - 2);
+}
+
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(0); cout.tie(0);
+
+    cin >> n;
+
+    int ans = Fibonacci(n);
+    cout << ans;
+
+    return 0;
+}
