1e02eccd307a74d93b53eb02efeaf17fad40ac00
hyunjung98 <hjk2754@gmail.com>
2023-03-06 15:12:45 +0900

    과제 안 내신 분..?

---
+++ BOJ/5597/5597.cpp
@@ -0,0 +1,30 @@
+﻿#include <iostream>
+#include <algorithm>
+
+using namespace std;
+
+int num;
+int students[31] = { 0, };
+int ans[2] = { 0, };
+
+int main()
+{
+	ios::sync_with_stdio(false);
+	cin.tie(0); cout.tie(0);
+
+	for (int i = 1; i <= 28; i++)
+	{
+		cin >> num;
+		students[num] = num;
+	}
+
+	for (int i = 1; i <= 30; i++)
+	{
+		if (students[i] == 0)
+		{
+			cout << i << '\n';
+		}
+	}
+
+    return 0;
+}
