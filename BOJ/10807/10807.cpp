ea2123b0fd507e9f5fba25a841a62d75729cef30
hyunjung98 <hjk2754@gmail.com>
2023-03-06 11:44:03 +0900

    개수 세기

---
+++ BOJ/10807/10807.cpp
@@ -0,0 +1,34 @@
+﻿#include <iostream>
+using namespace std;
+
+int n, v;
+int arr[101];
+
+int solve(int num)
+{
+    int cnt = 0;
+    for (int i = 0; i < n; i++)
+    {
+        if (num == arr[i])
+            cnt++;
+    }
+    return cnt;
+}
+
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(0); cout.tie(0);
+
+    cin >> n;
+    for (int i = 0; i < n; i++)
+    {
+        cin >> arr[i];
+    }
+
+    cin >> v;
+    int ans = solve(v);
+    cout << ans;
+
+    return 0;
+}
