a1195e50f486fcfbd9d7cbdddc1b31be0d16950d
hyunjung98 <hjk2754@gmail.com>
2023-03-06 18:55:40 +0900

    나머지

---
+++ BOJ/3052/3052.cpp
@@ -0,0 +1,28 @@
+﻿#include <iostream>
+#include <set>
+#include <vector>
+
+using namespace std;
+
+int cnt = 10;
+vector<int> v;
+
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(0); cout.tie(0);
+
+    int num = 0;
+    for (int i = 0; i < 10; i++)
+    {
+        cin >> num;
+        int element = num % 42;
+        v.push_back(element);
+    }
+    
+    set<int> s(v.begin(), v.end());
+
+    cout << s.size();
+
+    return 0;
+}
