907c8dfe6bfadc095721e95160a8d8b29c6f68ec
hyunjung98 <hjk2754@gmail.com>
2023-03-24 18:20:37 +0900

    셀프 넘버

---
+++ BOJ/4673/4673.cpp
@@ -0,0 +1,37 @@
+﻿#include <iostream>
+using namespace std;
+
+int func(int n)
+{
+	int selfNumber = n;
+	while (n != 0)
+	{
+		selfNumber += n % 10;
+		n = n / 10;
+	}
+	
+	return selfNumber;
+}
+
+int main()
+{
+	ios::sync_with_stdio(false);
+	cin.tie(0); cout.tie(0);
+
+	bool numbers[10001] = { false };
+	int temp = 0;
+	for (int i = 1; i <= 10000; i++)
+	{
+		temp = func(i);
+		if (temp <= 10000)
+			numbers[temp] = true;
+	}
+
+	for (int i = 1; i <= 10000; i++)
+	{
+		if (!numbers[i])
+			cout << i << '\n';
+	}
+
+	return 0;
+}
