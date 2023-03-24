b6c92319b310cffb7215ed789603ff6e0c8ca59d
hyunjung98 <hjk2754@gmail.com>
2023-03-24 18:42:21 +0900

    영화감독 숌

---
+++ BOJ/1436/1436.cpp
@@ -0,0 +1,29 @@
+﻿#include <iostream>
+#include <string>
+using namespace std;
+
+int main()
+{
+	ios::sync_with_stdio(false);
+	cin.tie(0); cout.tie(0);
+
+	int n;
+	cin >> n;
+
+	int title = 665;
+	int cnt = 0;
+
+	string str;
+
+	while (title++)
+	{
+		str = to_string(title);
+		if (str.find("666") != string::npos)
+			cnt++;
+		if (cnt == n) break;
+	}
+
+	cout << title;
+
+	return 0;
+}
