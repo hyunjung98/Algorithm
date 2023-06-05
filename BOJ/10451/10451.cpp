3dd9c3b160060af882f3e913b0c0e44d6949aa4a
hyunjung98 <hjk2754@gmail.com>
2023-06-05 21:15:16 +0900

    순열 사이클

---
+++ BOJ/10451/10451.cpp
@@ -0,0 +1,58 @@
+﻿#include <iostream>
+#include <vector>
+#include <cstring>
+
+#define MAX 1001
+
+using namespace std;
+
+vector<int> vec[MAX];
+bool visited[MAX];
+
+void dfs(int v)
+{
+    visited[v] = true;
+    for (int i = 0; i < vec[v].size(); i++)
+    {
+        int next = vec[v][i];
+        if (!visited[next]) dfs(next);
+    }
+}
+
+int main()
+{
+    cin.tie(0); cout.tie(0);
+    ios::sync_with_stdio(false);
+
+    int tc;
+    cin >> tc;
+    while (true)
+    {
+        if (tc == 0) break;
+        int size;
+        cin >> size;
+        int cnt = 0;
+        for (int i = 1; i <= size; i++)
+        {
+            int n;
+            cin >> n;
+            vec[i].push_back(n);
+            vec[n].push_back(i);
+        }
+
+        for (int i = 1; i <= size; i++)
+        {
+            if (!visited[i])
+            {
+                dfs(i);
+                cnt++;
+            }
+        }
+
+        cout << cnt << '\n';
+        memset(vec, 0, sizeof(vec));
+        memset(visited, 0, sizeof(visited));
+        tc--;
+    }
+    return 0;
+}
