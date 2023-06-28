697569d496cb20259dfb6a356d403a99025c99c8
hyunjung98 <hjk2754@gmail.com>
2023-06-28 15:37:09 +0900

    Create 19532.cpp

---
+++ BOJ/19532/19532.cpp
@@ -0,0 +1,25 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int a, b, c, d, e, f;
+
+int main()
+{
+	ios::sync_with_stdio(false);
+	cin.tie(0); cout.tie(0);
+
+	cin >> a >> b >> c >> d >> e >> f;
+
+	for (int x = -999; x <= 999; x++)
+	{
+		for (int y = -999; y <= 999; y++)
+		{
+			if ((a * x + b * y == c) && (d * x + e * y == f))
+			{
+				cout << x << ' ' << y;
+				return 0;
+			}
+		}
+	}
+}
